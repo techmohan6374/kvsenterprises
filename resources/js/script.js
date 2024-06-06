ea67071d0a3dd52a761bfda4c25da770eb19e10b
Tech Mohan <149706492+techmohan6374@users.noreply.github.com>
2024-06-06 13:02:15 +0530

    Update script.js
    js page changes

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,31 +1,35 @@
-// Loader Code
-$('#app').css('display', 'none');
-setTimeout(() => {
-    $('#app').css('display', 'block');
-    $('#loader').css('display', 'none');
-}, 2000);
+   // Loader Code
 
+        $('#app').css('display', 'none');
+        setTimeout(() => {
+            $('#app').css('display', 'block');
+            $('#loader').css('display', 'none');
+        }, 2000);
 
-var vm = new Vue({
-    el: '#app',
-    data: {
-
-    },
-    computed: {
-
-    },
-
-    methods: {
-        navBarShow() {
-            $("#navbarSupportedContent").toggleClass("show");
-        },
-    },
-
-    mounted() {
-        var elem = document.querySelector('.main-carousel');
-        var flkty = new Flickity(elem, {
-            cellAlign: 'center',
-            contain: true
+        $(document).ready(function () {
+            $('.navbar-nav>li>a').on('click', function () {
+                $('.navbar-collapse').collapse('hide');
+            });
         });
-    }
-});
+
+        var vm = new Vue({
+            el: '#app',
+            data: {
+
+            },
+            computed: {
+
+            },
+
+            methods: {
+
+            },
+
+            mounted() {
+                var elem = document.querySelector('.main-carousel');
+                var flkty = new Flickity(elem, {
+                    cellAlign: 'center',
+                    contain: true
+                });
+            }
+        });
